bd55d8e2e56a7c73ef5714c021c397ee2daeec90
Owen Block <block.o@wustl.edu>
2026-03-30 11:02:07 -0500

    squareclick for square

--- src/App.jsx
+++ src/App.jsx
@@ -201,3 +201,36 @@ export default function Board() {
     </>
   );
 }
+import { useState } from 'react';
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => {}} />
+        <Square value={squares[1]} onSquareClick={() => {}} />
+        <Square value={squares[2]} onSquareClick={() => {}} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => {}} />
+        <Square value={squares[4]} onSquareClick={() => {}} />
+        <Square value={squares[5]} onSquareClick={() => {}} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => {}} />
+        <Square value={squares[7]} onSquareClick={() => {}} />
+        <Square value={squares[8]} onSquareClick={() => {}} />
+      </div>
+    </>
+  );
+}
