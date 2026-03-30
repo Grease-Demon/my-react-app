ee3772fbeb474271a52821d3ab6bee9133672d18
Owen Block <block.o@wustl.edu>
2026-03-30 10:52:50 -0500

    wrap multiple squares

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
 export default function Square() {
   return <button className="square">X</button>;
 }
+export default function Square() {
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
+}
